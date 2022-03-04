ee0a67058ff5b4e3d0aaa4650feb584c68f80182
lwthiker <lwt@lwthiker.com>
2022-03-04 15:48:23 +0200

    Fix 'Accept' header of Microsoft Edge

--- lib/easy.c
+++ lib/easy.c
@@ -387,7 +387,7 @@ static const struct impersonate_opts {
       "sec-ch-ua-platform: \"Windows\"",
       "Upgrade-Insecure-Requests: 1",
       "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 Edg/98.0.1108.62",
-      "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
       "Sec-Fetch-Site: none",
       "Sec-Fetch-Mode: navigate",
       "Sec-Fetch-User: ?1",
