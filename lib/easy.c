351dba44d02524c0b5df84309db593bc31143662
lwthiker <lwt@lwthiker.com>
2022-03-04 15:53:40 +0200

    Fix Firefox impersonation HTTP headers

--- lib/easy.c
+++ lib/easy.c
@@ -323,15 +323,15 @@ static const struct impersonate_opts {
       "rsa_3des_ede_cbc_sha",
     .http_headers = {
       "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0",
-      "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+      "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
       "Accept-Language: en-US,en;q=0.5",
       "Accept-Encoding: gzip, deflate, br",
-      "Connection: keep-alive",
       "Upgrade-Insecure-Requests: 1",
       "Sec-Fetch-Dest: document",
       "Sec-Fetch-Mode: navigate",
       "Sec-Fetch-Site: none",
-      "Sec-Fetch-User: ?1"
+      "Sec-Fetch-User: ?1",
+      "TE: Trailers"
     }
   },
   {
@@ -361,12 +361,12 @@ static const struct impersonate_opts {
       "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
       "Accept-Language: en-US,en;q=0.5",
       "Accept-Encoding: gzip, deflate, br",
-      "Connection: keep-alive",
       "Upgrade-Insecure-Requests: 1",
       "Sec-Fetch-Dest: document",
       "Sec-Fetch-Mode: navigate",
       "Sec-Fetch-Site: none",
-      "Sec-Fetch-User: ?1"
+      "Sec-Fetch-User: ?1",
+      "TE: Trailers"
     }
   }
 };
