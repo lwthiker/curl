9c784bb2c138c357bd049ce3984c6ec60d3dca9a
lwthiker <lwt@lwthiker.com>
2022-05-15 19:31:23 +0300

    Remove impersonation support for Chrome & Edge 98
    Chrome 98 (and Edge 98, which is based on it) used a non-standard HTTP2
    SETTINGS frame. Newer versions do not use it anymore. Instead of
    supporting both, which requires yet another libcurl option, just remove
    support for the older browser versions.

--- lib/easy.c
+++ lib/easy.c
@@ -317,46 +317,6 @@ static const struct impersonate_opts {
    * configurable through curl_easy_setopt() */
 } impersonations[] = {
   {
-    .target = "chrome98",
-    .httpversion = CURL_HTTP_VERSION_2_0,
-    .ssl_version = CURL_SSLVERSION_TLSv1_2 | CURL_SSLVERSION_MAX_DEFAULT,
-    .ciphers =
-      "TLS_AES_128_GCM_SHA256,"
-      "TLS_AES_256_GCM_SHA384,"
-      "TLS_CHACHA20_POLY1305_SHA256,"
-      "ECDHE-ECDSA-AES128-GCM-SHA256,"
-      "ECDHE-RSA-AES128-GCM-SHA256,"
-      "ECDHE-ECDSA-AES256-GCM-SHA384,"
-      "ECDHE-RSA-AES256-GCM-SHA384,"
-      "ECDHE-ECDSA-CHACHA20-POLY1305,"
-      "ECDHE-RSA-CHACHA20-POLY1305,"
-      "ECDHE-RSA-AES128-SHA,"
-      "ECDHE-RSA-AES256-SHA,"
-      "AES128-GCM-SHA256,"
-      "AES256-GCM-SHA384,"
-      "AES128-SHA,"
-      "AES256-SHA",
-    .npn = false,
-    .alpn = true,
-    .alps = true,
-    .tls_session_ticket = true,
-    .cert_compression = "brotli",
-    .http_headers = {
-      "sec-ch-ua: \" Not A;Brand\";v=\"99\", \"Chromium\";v=\"98\", \"Google Chrome\";v=\"98\"",
-      "sec-ch-ua-mobile: ?0",
-      "sec-ch-ua-platform: \"Windows\"",
-      "Upgrade-Insecure-Requests: 1",
-      "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
-      "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-      "Sec-Fetch-Site: none",
-      "Sec-Fetch-Mode: navigate",
-      "Sec-Fetch-User: ?1",
-      "Sec-Fetch-Dest: document",
-      "Accept-Encoding: gzip, deflate, br",
-      "Accept-Language: en-US,en;q=0.9"
-    }
-  },
-  {
     .target = "chrome99",
     .httpversion = CURL_HTTP_VERSION_2_0,
     .ssl_version = CURL_SSLVERSION_TLSv1_2 | CURL_SSLVERSION_MAX_DEFAULT,
@@ -508,46 +468,6 @@ static const struct impersonate_opts {
       "Upgrade-Insecure-Requests: 1",
       "User-Agent: Mozilla/5.0 (Linux; Android 12; Pixel 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.58 Mobile Safari/537.36",
       "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-      "Sec-Fetch-Site: none",
-      "Sec-Fetch-Mode: navigate",
-      "Sec-Fetch-User: ?1",
-      "Sec-Fetch-Dest: document",
-      "Accept-Encoding: gzip, deflate, br",
-      "Accept-Language: en-US,en;q=0.9"
-    }
-  },
-  {
-    .target = "edge98",
-    .httpversion = CURL_HTTP_VERSION_2_0,
-    .ssl_version = CURL_SSLVERSION_TLSv1_2 | CURL_SSLVERSION_MAX_DEFAULT,
-    .ciphers =
-      "TLS_AES_128_GCM_SHA256,"
-      "TLS_AES_256_GCM_SHA384,"
-      "TLS_CHACHA20_POLY1305_SHA256,"
-      "ECDHE-ECDSA-AES128-GCM-SHA256,"
-      "ECDHE-RSA-AES128-GCM-SHA256,"
-      "ECDHE-ECDSA-AES256-GCM-SHA384,"
-      "ECDHE-RSA-AES256-GCM-SHA384,"
-      "ECDHE-ECDSA-CHACHA20-POLY1305,"
-      "ECDHE-RSA-CHACHA20-POLY1305,"
-      "ECDHE-RSA-AES128-SHA,"
-      "ECDHE-RSA-AES256-SHA,"
-      "AES128-GCM-SHA256,"
-      "AES256-GCM-SHA384,"
-      "AES128-SHA,"
-      "AES256-SHA",
-    .npn = false,
-    .alpn = true,
-    .alps = true,
-    .tls_session_ticket = true,
-    .cert_compression = "brotli",
-    .http_headers = {
-      "sec-ch-ua: \" Not A;Brand\";v=\"99\", \"Chromium\";v=\"98\", \"Microsoft Edge\";v=\"98\"",
-      "sec-ch-ua-mobile: ?0",
-      "sec-ch-ua-platform: \"Windows\"",
-      "Upgrade-Insecure-Requests: 1",
-      "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 Edg/98.0.1108.62",
-      "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
       "Sec-Fetch-Site: none",
       "Sec-Fetch-Mode: navigate",
       "Sec-Fetch-User: ?1",
